9a77f7e96846206963951e66d9e8f984566233c3
Juan Carlos <rapidclimate@yahoo.com>
2024-10-22 12:34:19 -0600

    Fundamentos de programacion

--- src/unidad1/Bienvenido.java
+++ src/unidad1/Bienvenido.java
@@ -1,8 +1,16 @@
 package unidad1;
 
+import java.util.Scanner;
+
 public class Bienvenido {
 
     public static void main(String[] args) {
         System.out.println("Homa mundo");
+        System.out.println("Introduce una palabra seguida de enter:");
+        var in=new Scanner(System.in);
+        var palabra=in.next();
+        System.out.println("La palabra que escribiste es:"+palabra);
+
+
     }
 }
