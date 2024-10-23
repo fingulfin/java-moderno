b3a96582cd34167206b7234977f0e201994917f7
Juan Carlos <rapidclimate@yahoo.com>
2024-10-23 15:19:10 -0600

    Fundamentos de programacion

--- src/unidad1/Operadores.java
+++ src/unidad1/Operadores.java
@@ -4,8 +4,24 @@ public class Operadores {
     public static void main(String[] args) {
 
         var x=0;
-        //Operador unario ++
+        //Ejemplo de operador unario ++ y el operando es la variable x.
         x++;
+       //Ejemplo de operador binario es la + donde los operandos son el 5 y el 10
+       var y=+5+10;
+
+       //Ejemplo de operador Ternario condicional, el resultado final se asigna a lo que desees.
+       var a=5;
+       var b=6;
+       var c=10;
+
+     c=  a<b? a:c;
+
+     //Otro ejemplo de Ternario
+        int num1 = 50;
+        int num2 = 25;
+    // Asignamos  num2 a minNum, porque num2 es menor que num1
+        int minNum = (num1 < num2 ? num1 : num2);
+
 
     }
 }
