1b951c5d391a5e6d2c4f2c0d3c59fd62bb2400e4
Juan Carlos <rapidclimate@yahoo.com>
2024-10-28 11:24:30 -0600

    Fundamentos de programacion

--- src/unidad1/ArreglosUno.java
+++ src/unidad1/ArreglosUno.java
@@ -1,10 +1,29 @@
 package unidad1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class ArreglosUno {
     public static void main(String[] args) {
-        int[] empId = new int[5];
+        int[] numeros = {8,2,6,3,-4,5,0};
+       String[]nombres={"Juan", "Pedro", "Zacarias", "Armando", "Gerardo", "Roberto"};
 
-    }
+       Trabajador[]trabajadores={new Trabajador("Juan",2000),new Trabajador("Arturo", 1500), new Trabajador("Zacarias", 5500)};
+       Arrays.sort(trabajadores,Comparator.comparing(Trabajador::getSueldo).reversed());
+
+        List<String> frutas=new ArrayList<>();
+   //  Arrays.sort(, Comparator.reverseOrder());
+     for(Trabajador t: trabajadores){
+         System.out.println(t);
+     }
+
+
+
+        }
+
+
+
+
 }
---
+++ src/unidad1/Trabajador.java
@@ -0,0 +1,38 @@
+package unidad1;
+
+public class Trabajador {
+    String nombre;
+    float sueldo;
+
+    public Trabajador() {
+    }
+
+    @Override
+    public String toString() {
+        return "Trabajador{" +
+                "nombre='" + nombre + '\'' +
+                ", sueldo=" + sueldo +
+                '}';
+    }
+
+    public Trabajador(String nombre, float sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
+}
